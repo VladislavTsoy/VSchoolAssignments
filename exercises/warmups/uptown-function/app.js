fe43f45f2e279ae3bf242176ff223261f019b040
Vladislav <vit3@rutgers.edu>
2018-07-25 09:26:50 -0600

    post pie and beer

---
+++ exercises/warmups/uptown-function/app.js
@@ -0,0 +1,42 @@
+var lyrics = ["This", "hit", "that", "ice", "cold",  
+              "Michelle", "Pfeiffer", "that", "white", 
+              "gold", "This", "one", "for", "them", 
+              "hood", "girls", "Them", "good", "girls", 
+              "straight", "masterpieces", "Stylin'", 
+              "whilen'", "livin'", "it", "up", "in", 
+              "the", "city", "Got", "Chucks", "on", 
+              "with", "Saint", "Laurent", "Gotta", "kiss", 
+              "myself", "I'm", "so", "pretty"];
+
+
+function uptown1(arr){
+    console.log(arr.join(' ')) 
+}
+
+function uptown2(arr){
+    arr.reverse();
+    console.log(arr.join(' '));
+}
+
+function uptown3(arr){
+    var result = [];
+    for(var i = 0; i < arr.length; i+=2){
+        result.push(arr[i]);
+    }
+    console.log(result.join(' '));
+}
+
+function uptown4(arr){
+    for(var i = 0; i < arr.length; i+=2){
+        if(i != arr.length - 1){
+            var temp = arr[i];
+            arr[i] = arr[i+1]
+            arr[i+1] = temp;
+        }
+    }
+    console.log(arr.join(' '));
+}
+
+uptown4(lyrics);
+
+
--- projects/week-6/map.js
+++ projects/week-6/map.js
@@ -1,29 +1,91 @@
 var map;
-function initMap() {
-    var myLatLong = {lat: 40.7608, lng: -111.8910}
-    var mapOptions = {
+// var myLatLong = {lat: 40.7608, lng: -111.8910}
+//     var mapOptions = {
+//         center: {
+//             lat: 40.7608, 
+//             lng: -111.8910
+//         },
+//         zoom: 12
+//     }
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 40.7608, 
             lng: -111.8910
         },
-        zoom: 12
-    }
-    
-    map = new google.maps.Map(document.getElementById('map'), 
-    {
-    center: myLatLong,
-    zoom: 12
+      zoom: 15,
+      mapTypeId: 'roadmap'
     });
-
+    var myLatLng = {
+      lat: 40.7608, 
+      lng: -111.8910
+    };
     var marker = new google.maps.Marker({
-        position: myLatLong,
-        map: map,
-        title: 'Hello, World!'
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
     });
 
- 
-}
-init();
+    var markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+
 
 
 
